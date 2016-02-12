393d7e0e4da264a52efd8272d5b6bf748456cc8f
Marcelo Martins Maia do Couto <marcelo.m.maia@gmail.com>
2016-02-12 00:21:07 -0200

    * Correção da rotina main

--- FTPUploader_bootloader/src/FTPUploader_bootloader.c
+++ FTPUploader_bootloader/src/FTPUploader_bootloader.c
@@ -13,22 +13,133 @@
 #endif
 
 #include <cr_section_macros.h>
+#include <NXP/crp.h>
 
-#include <stdio.h>
+#include <string.h>
+#include "lpc17xx_emac.h"
 
-// TODO: insert other include files here
+#include "config.h"
+#include "timer.h"
+#include "uip.h"
+#include "uip_arp.h"
+#include "tapdev.h"
+#include "diagnostics.h"
+#include "bootloaderapp.h"
 
-// TODO: insert other definitions and declarations here
+
+#define BUF ((struct uip_eth_hdr *)&uip_buf[0])
+
+/*--------------------------- uip_log ---------------------------------*/
+
+void uip_log(char *m)
+{
+  //printf("uIP log message: %s\n", m);
+}
+
+/*--------------------------- main ---------------------------------*/
+
+unsigned int i;
+uip_ipaddr_t ipaddr;	/* local IP address */
+struct timer periodic_timer, arp_timer, boot_timer;
 
 int main(void) {
 
-    printf("Hello World\n");
+	// clock init
+	clock_init();
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
-    while(1) {
-        i++ ;
-    }
-    return 0 ;
+	// two timers for tcp/ip
+	timer_set(&periodic_timer, CLOCK_SECOND / 2); /* 0.5s */
+	timer_set(&arp_timer, CLOCK_SECOND * 10);	/* 10s */
+
+	diag_init();
+
+	// ethernet init
+	tapdev_init();
+
+	// Initialize the uIP TCP/IP stack.
+	uip_init();
+
+	uip_ipaddr(ipaddr, MYIP1,MYIP2,MYIP3,MYIP4);
+	uip_sethostaddr(ipaddr);
+	uip_ipaddr(ipaddr, DRTR1,DRTR2,DRTR3,DRTR4);
+	uip_setdraddr(ipaddr);
+	uip_ipaddr(ipaddr, SMSK1, SMSK2, SMSK3, SMSK4);
+    uip_setnetmask(ipaddr);
+
+	bl_init();
+
+	while(1)
+	{
+		diag_appcall();
+		bl_appcall();
+
+		/* receive packet and put in uip_buf */
+		uip_len = tapdev_read(uip_buf);
+    	if(uip_len > 0)		/* received packet */
+    	{
+      		if(BUF->type == htons(UIP_ETHTYPE_IP))	/* IP packet */
+      		{
+	      		uip_arp_ipin();
+	      		uip_input();
+	      		/* If the above function invocation resulted in data that
+	         		should be sent out on the network, the global variable
+	         		uip_len is set to a value > 0. */
+
+	      		if(uip_len > 0)
+        		{
+	      			uip_arp_out();
+	        		tapdev_send(uip_buf,uip_len);
+	      		}
+      		}
+	      	else if(BUF->type == htons(UIP_ETHTYPE_ARP))	/*ARP packet */
+	      	{
+	        	uip_arp_arpin();
+		      	/* If the above function invocation resulted in data that
+		         	should be sent out on the network, the global variable
+		         	uip_len is set to a value > 0. */
+		      	if(uip_len > 0)
+	        	{
+		        	tapdev_send(uip_buf,uip_len);	/* ARP ack*/
+		      	}
+	      	}
+    	}
+    	else if(timer_expired(&periodic_timer))	/* no packet but periodic_timer time out (0.5s)*/
+    	{
+      		timer_reset(&periodic_timer);
+
+      		for(i = 0; i < UIP_CONNS; i++)
+      		{
+      			uip_periodic(i);
+		        /* If the above function invocation resulted in data that
+		           should be sent out on the network, the global variable
+		           uip_len is set to a value > 0. */
+		        if(uip_len > 0)
+		        {
+		          uip_arp_out();
+		          tapdev_send(uip_buf,uip_len);
+		        }
+      		}
+#if UIP_UDP
+			for(i = 0; i < UIP_UDP_CONNS; i++) {
+				uip_udp_periodic(i);
+				/* If the above function invocation resulted in data that
+				   should be sent out on the network, the global variable
+				   uip_len is set to a value > 0. */
+				if(uip_len > 0) {
+				  uip_arp_out();
+				  tapdev_send(uip_buf,uip_len);
+				}
+			}
+#endif /* UIP_UDP */
+	     	/* Call the ARP timer function every 10 seconds. */
+			if(timer_expired(&arp_timer))
+			{
+				timer_reset(&arp_timer);
+				uip_arp_timer();
+			}
+    	}
+
+
+	}
+	return 0 ;
 }
